592343a93da6bedac410cfeaf4777a04738789ea
Yyffii <jonaadee98@gmail.com>
2022-10-23 22:34:08 -0600

    initial setup

---
+++ main.js
@@ -0,0 +1,6 @@
+var canvas = document.getElementById('canvas');
+var divHeight = document.getElementById('game').clientHeight;
+var divWidth = document.getElementById('game').clientWidth
+
+canvas.height = divHeight;
+canvas.width = divWidth;
